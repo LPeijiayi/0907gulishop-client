8829c716522a2f65897562dd492d9bd916554b82
LPeijiayi <1443817236@qq.com>
2022-09-07 17:30:15 +0800

    first

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import Vue from 'vue'
+import App from '@/App'
+
+Vue.config.productionTip = false
+
+// new Vue ({
+//   render: h => h(App)
+// }).$mount('#app')
+
+new Vue({
+  el:'#app',
+  render:h =>h(App)
+})
